501370bd9a08e3bd5f982733d7dbab8d50fd345f
Chris Donadeo <Chris.Donadeo@Aspect.com>
2017-05-15 19:20:14 -0400

    Add some controllers and views and stuff

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ActiveRecord::Base
   has_many :thing_group_memberships
   has_many :things, through: :thing_group_memberships
+
+  def to_s
+    "Group #{id}"
+  end
 end
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,4 +1,8 @@
 class Thing < ActiveRecord::Base
   has_many :thing_group_memberships
   has_many :groups, through: :thing_group_memberships
+
+  def to_s
+    "Thing #{id}"
+  end
 end
